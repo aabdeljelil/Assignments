7002863d99fd173b466a97bed4dcb5e28506fd07
Anissa Abdel-Jelil <aabdeljelil@hds.harvard.edu>
2020-01-11 22:46:32 -0500

    add functinos to calculator

---
+++ calculator.py
@@ -0,0 +1,15 @@
+# This part of the code defines a function
+def multiply(a,b):
+    return a * b
+
+# This part of the code defines a function
+def sum(a,b):
+    return a + b
+
+# This part of the code defines a function
+def subtract(a,b):
+    return a - b
+
+# This part of the code defines a function
+def divide(a,b):
+    return a / b
