9b41736d421d843a5c5aecca47d1711bd1ebf1c4
Anissa Abdel-Jelil <aabdeljelil@hds.harvard.edu>
2020-01-11 22:56:29 -0500

    update calc to call function and print explanation

--- calculator.py
+++ calculator.py
@@ -13,3 +13,11 @@ def subtract(a,b):
 # This part of the code defines a function
 def divide(a,b):
     return a / b
+
+# This part of the code then calls that function and assigns the variable x to its return value
+x = multiply(8,9)
+
+# This part of the code prints the multiplication function 
+print("I'm going to use the calculator functions to multiply 5 and 6")
+x = multiply(5,6)
+print(x)
